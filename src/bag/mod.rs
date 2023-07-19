38104b263b4278496e52575033d83c54d0441d11
Paul Duvenage <duvenagep@gmail.com>
2023-07-19 21:56:43 +0200

    feat(rust): Added Enum Geometries & Bagobject Types, moved everything to a lib file

--- src/bag/mod.rs
+++ src/bag/mod.rs
@@ -1,8 +1,1 @@
 pub mod lib;
-pub mod lig;
-pub mod num;
-pub mod opr;
-pub mod pnd;
-pub mod sta;
-pub mod vbo;
-pub mod wpl;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+#![allow(unused)]
+
 mod args;
 mod bag;
 mod helpers;
 mod work_dir;
 
+use crate::bag::lib::*;
 use args::{BagObjects, EntityType, LVBAGSubCommand, NLExtractArgs};
 use clap::Parser;
 use helpers::zip_seek::read_nested_zip;
+use rayon::prelude::*;
+use std::sync::{Arc, Mutex};
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;
 use std::time::Instant;
 use work_dir::new_folder;
 
@@ -27,11 +34,30 @@ fn main() {
             }
 
             LVBAGSubCommand::Parse(parse) => {
+                let _output_folder = new_folder("output");
+
                 println!("{:?}", &parse);
                 let path = &parse.file;
 
                 match parse.bag_object {
-                    None => println!("Parsing all"),
+                    None => {
+                        println!("Parsing all");
+                        let obj = vec![
+                            BagObjects::OPR,
+                            BagObjects::WPL,
+                            BagObjects::LIG,
+                            BagObjects::NUM,
+                            BagObjects::STA,
+                            BagObjects::VBO,
+                            BagObjects::PND,
+                        ];
+
+
+                        let _parse = obj
+                            .into_iter()
+                            .for_each(|o| { read_nested_zip(&path, o.to_string()); });
+
+                    }
                     Some(list) => {
                         let set: HashSet<_> = list.clone().into_iter().collect();
 
@@ -39,31 +65,32 @@ fn main() {
                             match l {
                                 BagObjects::LIG => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::LIG.to_string());
                                 }
                                 BagObjects::NUM => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::NUM.to_string());
                                 }
                                 BagObjects::STA => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::STA.to_string());
+                                    println!("{:?}", _r);
                                 }
                                 BagObjects::WPL => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::WPL.to_string());
                                 }
                                 BagObjects::PND => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::PND.to_string());
                                 }
                                 BagObjects::VBO => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::VBO.to_string());
                                 }
                                 BagObjects::OPR => {
                                     println!("Parsing {:?}", l);
-                                    let r = read_nested_zip(&path);
+                                    let _r = read_nested_zip(&path, BagObjects::OPR.to_string());
                                 }
                             }
                         }
