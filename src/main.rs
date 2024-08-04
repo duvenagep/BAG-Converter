53755f81037caa28ed977131a90b17b75b1cdd8e
Paul Duvenage <duvenagep@gmail.com>
2024-08-04 21:48:00 +0200

    feat(api): Changes to Clippy rules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // #![allow(unused)]
-#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+// #![warn(clippy::all, clippy::pedantic, clippy::restriction)]
 // #![warn(missing_docs)]
 
 mod args;
